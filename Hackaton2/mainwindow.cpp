5030acd38131109b9b53bc9f9d03b4fedbffb276
IsakNaslund <isak.naslund@gmail.com>
2016-01-23 17:11:01 +0100

    Added Loop

--- Hackaton2/mainwindow.cpp
+++ Hackaton2/mainwindow.cpp
@@ -21,7 +21,10 @@ void MainWindow::on_iButton_clicked()
 {
     Isaks isak;
 
-    ui->textBrowser->append(isak.Text());
+    for (int i=0; i<isak.Number(); i++)
+    {
+        ui->textBrowser->append(isak.Text());
+    }
 }
 
 void MainWindow::on_cButton_clicked()
