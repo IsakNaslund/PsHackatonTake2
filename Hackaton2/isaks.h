89477b5fe9f2eb0eb4ee87e8dbe622bd80aba81d
IsakNaslund <isak.naslund@gmail.com>
2016-01-23 16:43:19 +0100

    Added Isaks class,

---
+++ Hackaton2/isaks.h
@@ -0,0 +1,16 @@
+#ifndef ISAKS_H
+#define ISAKS_H
+
+#include "qstring.h"
+
+class Isaks
+{
+    int a;
+    QString text;
+public:
+    Isaks();
+    int Number();
+    QString Text();
+};
+
+#endif // ISAKS_H
